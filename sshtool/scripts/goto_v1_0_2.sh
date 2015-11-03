b43b9673ede4ada56a5f8302fc518cec5c1794fe
Teddy Boot <teddy.boot@sogeti.nl>
2015-11-03 13:46:22 +0100

    changed minor typo

--- sshtool/scripts/goto_v1_0_2.sh
+++ sshtool/scripts/goto_v1_0_2.sh
@@ -168,7 +168,7 @@ function _determineServerName () {
 function _sshConnect() {
 	_determinePort
 	_determineServerName
-	sshParameters="-X -v ${userNames[$userID]}@${servName}.${serverDomain}"
+	sshParameters="-X ${userNames[$userID]}@${servName}.${serverDomain}"
 	[[ -z ${portNr} ]] || sshParameters="$sshParameters -p ${portNr}"
 	clear
 	#echo "${SSH} ${sshParameters}"
